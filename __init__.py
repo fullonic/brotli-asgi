9f0f20d098c4ec76830eeeca854da3a3f4b60580
fullonic <somnium@riseup.net>
2020-05-27 10:18:23 +0200

    Create setup file for distribution

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from .brotli_middleware import BrotliMiddleware
---
+++ setup.py
@@ -0,0 +1,29 @@
+"""A compression AGSI middleware using brotli.
+
+Built using starlette under the hood, it can be used as a drop in replacement to
+GZipMiddleware for Starlette or FastAPI.
+"""
+
+from setuptools import setup  # type: ignore
+
+setup(
+    name="brotli_middleware",
+    version="0.1.1",
+    url="",
+    license="",
+    author="Diogo B Freitas",
+    author_email="somnium@riseup.net",
+    description="A compression AGSI middleware using brotli",
+    long_description=__doc__,
+    packages=["brotli_middleware"],
+    python_requires=">=3.5",
+    include_package_data=True,
+    install_requires=["starlette>=0.13.4", "brotli>=1.0.7"],
+    platforms="any",
+    zip_safe=False,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+)
